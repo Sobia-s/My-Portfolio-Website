583faad8bf1c699b3b7f9ae1f3d540edeee00b2f
My-First-Account <sobiasoomro97@gmail.com>
2024-06-26 09:37:29 +0500

    making some changes in footer, index.js and uninstall unnecessary libraries

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -6,23 +6,27 @@ export function Footer() {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <div className="footer-left">
-                    <h2 className="footer-heading">SOBIA SOOMRO</h2>
-                    <p className="footer-para">Certified Front-End Developer | Passionate about Creating Outstanding User Experiences</p>
-                </div>
-                <div className="footer-right">
-                    <h2 className="footer-heading">SOCIAL</h2>
-                    <div className="social-icons">
-                        <a href="https://github.com/Sobia-s" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-                    </div>
-                </div>
-            </div>
-            <div className="footer-bottom">
-                <p>© Copyright {new Date().getFullYear()}. Made by <a href="https://sobia-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">Sobia Soomro</a></p>
-            </div>
-        </footer>
+                 <div className="footer-left">
+                     <h3 id="footer-name">SOBIA SOOMRO</h3>
+                     <p className="footer-para">Certified Front-End Developer | Passionate about Creating Outstanding User Experiences</p>
+                 </div>
+                 <div className="footer-right">
+                     <h3 id="footer-socials">SOCIAL</h3>
+                     <div className="social-icons">
+                         <a href="https://github.com/Sobia-s" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                     </div>
+                 </div>
+             </div>
+             <div className="footer-bottom">
+                 <p>© Copyright {new Date().getFullYear()}. Made by <a href="https://sobia-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">Sobia Soomro</a></p>
+             </div>
+         </footer>
     );
 }
+
+
+
+
 
 
 
