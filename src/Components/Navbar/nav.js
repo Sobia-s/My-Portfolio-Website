9e4849a8ff114e0db800921eafc293b1727b1e5d
My-First-Account <sobiasoomro97@gmail.com>
2024-07-04 09:18:27 +0500

    add linkedIn in footer and remove github from navbar

--- src/Components/Navbar/nav.js
+++ src/Components/Navbar/nav.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
-import { FaGithub } from "react-icons/fa";
 import logo from './logo.png';
 import "./nav.css";
 
@@ -8,30 +7,15 @@ export function Nav() {
     const [isOpen, setIsOpen] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
 
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         setIsMobile(window.innerWidth <= 768);
-    //     };
-
-    //     window.addEventListener('resize', handleResize);
-
-    //     handleResize();
-
-    //     return () => window.removeEventListener('resize', handleResize);
-    // }, []);
-
     useEffect(() => {
         const handleResize = () => {
             setIsMobile(window.innerWidth <= 768);
         };
 
-        // Add event listener on component mount
         window.addEventListener('resize', handleResize);
 
-        // Initial call to set initial mobile state
         handleResize();
 
-        // Cleanup: remove event listener on component unmount
         return () => {
             window.removeEventListener('resize', handleResize);
         };
@@ -63,7 +47,6 @@ export function Nav() {
                 <li><Link to="projects" smooth={true} duration={500} onClick={closeMenu}>PROJECTS</Link></li>
                 <li><Link to="certificate" smooth={true} duration={500} onClick={closeMenu}>CERTIFICATES</Link></li>
                 <li><Link to="contact" smooth={true} duration={500} onClick={closeMenu}>CONTACT</Link></li>
-                <li><a href="https://github.com/Sobia-s" target="_blank" rel="noopener noreferrer"><FaGithub size={24} /></a></li>
             </ul>
         </nav>
     );
