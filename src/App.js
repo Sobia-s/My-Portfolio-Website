f496ff998e0b14f00810f3f7d4ecf4df9f1fe9ab
My-First-Account <sobiasoomro97@gmail.com>
2024-07-09 18:14:23 +0500

    adding analytics code in index.html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { Home } from './Home';
-import { useEffect } from 'react';
-import ReactGA from 'react-ga';
 
 
 function App() {
-
-    useEffect(() => {
-      console.log('Initializing Google Analytics');
-      ReactGA.initialize('G-947YF0EGRF');
-      ReactGA.pageview(window.location.pathname + window.location.search);
-    }, []);
-
     return (
       <div className="App">
         <Home />
